4d2cedd699672b1461063680f961bf6be046b2a5
LobotomyInfoStorehouse <LobotomyInfoStorehouse@gmail.com>
2018-11-14 00:34:47 +0900

    f

--- backend/api.py
+++ backend/api.py
@@ -26,5 +26,5 @@ def get_sephirah():
 def get_sephirah_detail(name):
     sephirah_data = open("./backend/data/sephirah/sephirah_frof.json", 'r',encoding="utf-8")
     data = json.load(sephirah_data)
-    info =data[name]
+    info =data["Angela"]
     return jsonify(info)
